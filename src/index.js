45b77e4efea161729561ed24015f8bd7bb3dde5a
georhiireva <47509262+georhiireva@users.noreply.github.com>
2024-01-06 16:38:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <head>
-      <script src="https://telegram.org/js/telegram-web-app.js"></script>
-    </head>
     <App />
   </React.StrictMode>
 );
