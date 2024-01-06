06120b9ee7b1e5129a833f5563cd392ea45b12a9
georhiireva <47509262+georhiireva@users.noreply.github.com>
2024-01-06 15:51:10 +0300

    Integrate with telegram

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <head>
+      <script src="https://telegram.org/js/telegram-web-app.js" />
+    </head>
     <App />
   </React.StrictMode>
 );
